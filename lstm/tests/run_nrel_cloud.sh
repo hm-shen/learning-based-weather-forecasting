f0a938ba65022cf66d241f1f455ad8c1b2df595f
hm-shen <hmshen@umich.edu>
2018-04-06 16:47:47 -0400

    adjust directories

--- lstm/tests/run_nrel_cloud.sh
+++ lstm/tests/run_nrel_cloud.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # run cloud fraction prediction on HOURLY NREL data
-python driver.py\
-       -p ../data/NREL_total_1314_train.mat\
-       -f average\
-       -m cloud\
-       -o ../outputs/\
-       -n NREL1314\
-       -s 1
+(cd ../src/ && python driver.py\
+                      -p ../data/NREL_total_1314_train.mat\
+                      -f average\
+                      -m cloud\
+                      -o ../outputs/\
+                      -n NREL1314\
+                      -s 1)
--- lstm/tests/run_wrf_solar.sh
+++ lstm/tests/run_wrf_solar.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # use LSTM model to forecast solar irradiance using WRF dataset
-python driver.py\
-       -p ../data/WRF_data_2017_Jan_to_Mar.mat\
-       -f average\
-       -m solar\
-       -o ../outputs/\
-       -n WRF_Jan_to_Mar\
-       -s 1
+(cd ../src/ && python driver.py\
+                      -p ../data/WRF_data_2017_Jan_to_Mar.mat\
+                      -f average\
+                      -m solar\
+                      -o ../outputs/\
+                      -n WRF_Jan_to_Mar\
+                      -s 1)
